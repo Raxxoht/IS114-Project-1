70f2fc9900e12a18a261b8aef8b6ece6c03fcb77
Renate Westermann <renatw17@uia.no>
2021-10-01 22:45:57 +0200

    fjernet ekstern js fil og lagt det inn i html fil, tullet litt med css

--- script.js
+++
@@ -1,8 +0,0 @@
-let sliders = document.getElementsByClassName("slider");
-let verdi = document.getElementsByClassName("viv");
-
-for(i=0;i!=sliders.length;i++){
-    sliders[i].oninput = function () {
-        verdi[Number(this.getAttribute('data-value'))].innerHTML = this.value;
-    }
-}
